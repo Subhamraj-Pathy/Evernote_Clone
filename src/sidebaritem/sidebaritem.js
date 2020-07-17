3b13ee1eae1b244eb5fc3b4926cec5a40878a5ad
Subhamraj-Pathy <subhamrajpathy@gmail.com>
2020-07-18 01:52:15 +0530

    Sidebar Item Component Primary

--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -5,17 +5,32 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { removeHTMLTags } from '../helpers';
+import { List } from '@material-ui/core';
 
 class SidebarItemComponent  extends Component {
-    
-    constructor() {
-        super()
-      }
+
+    selectNote = (n, i) => {
+        this.props.selectNote(n, i)
+    }
+
+    deleteNote = (note) => {
+        if(window.confirm(`Do You Want To Delete The Note : ' ${note.title} ' ?`)) {
+            this.props.deleteNote(note)
+        }
+    }
 
     render() {
+
+        const { _note, _index, selectedNoteIndex, classes } = this.props
+
         return (
-            <div>
-                Hello From SidebarItem
+            <div key={_index}>
+                <ListItem className={classes.listItem} selected={selectedNoteIndex === _index} alignItems='flex-start'>
+                    <div className={classes.textSection} onClick={() => this.selectNote(_note, _index)}>
+                        <ListItemText primary={_note.title} secondary={removeHTMLTags(_note.body.substring(0,30) + '...')}></ListItemText>
+                    </div>
+                    <DeleteIcon onClick={() => this.deleteNote(_note)} className={classes.deleteIcon}></DeleteIcon>
+                </ListItem>
             </div>
         );
     }
